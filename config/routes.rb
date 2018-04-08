4878a524d48312c6bc41fed6d5762d56f954fbff
@FYoshi0118 <yoshiki.f0118@gmail.com>
2018-04-08 22:11:03 +0900

    Editのルートでどハマり中。.7て.がついてrouteエラーなる

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  get '/top' => "root#top"
+  get 'books/new'
+
+  get '/top' => 'root#top'
+  get '/books' => 'books#index', as: 'book'
+  post '/books' => 'books#create'
+  get '/books/:id' => 'books#show', as: 'show_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch '/books/:id' => 'books#update', as: 'update_book'
+  put '/books/:id' => 'books#update', as: 'update_book'
+  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
 end
